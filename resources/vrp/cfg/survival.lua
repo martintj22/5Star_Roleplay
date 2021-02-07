87784056305d419a47082e5037817ef1a2a63b53
Satoz <satoz.game@gmail.com>
2021-02-07 16:50:53 +0100

    Food and minimap

--- resources/vrp/cfg/survival.lua
+++ resources/vrp/cfg/survival.lua
@@ -1,9 +1,9 @@
 
 local cfg = {
-  vital_display = true,
+  vital_display = false,
   vital_display_anchor = "minimap",
-  water_per_minute = 0.025,
-  food_per_minute = 0.0125,
+  water_per_minute = 0.000,
+  food_per_minute = 0.000,
   overflow_damage_factor = 2,
   pvp = true,
   police = false

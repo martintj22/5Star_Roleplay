d9cc3e1c841d31a4eb81e54001d81a8fd5ebe72f
Martin Jørgensen <satoz.game@gmail.com>
2021-02-07 20:05:28 +0100

    No hunger

--- resources/vrp/cfg/survival.lua
+++ resources/vrp/cfg/survival.lua
@@ -2,8 +2,8 @@
 local cfg = {
   vital_display = true,
   vital_display_anchor = "minimap",
-  water_per_minute = 0.025,
-  food_per_minute = 0.0125,
+  water_per_minute = 0.000,
+  food_per_minute = 0.0000,
   overflow_damage_factor = 2,
   pvp = true,
   police = false
